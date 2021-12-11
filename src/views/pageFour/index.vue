a214deb5e780786c1d37983b3c0655451c0db6d7
Drinker <424160958@qq.com>
2021-12-11 20:21:09 +0800

    change

--- src/views/pageFour/index.vue
+++ src/views/pageFour/index.vue
@@ -1,20 +1,47 @@
 <template>
-  <el-table
-      :data="[{ name : 'Michale' }]"
-  >
-    <el-table-column prop="name" label="名字"></el-table-column>
-  </el-table>
+ <div class="table-container">
+   <el-table
+       :data="tableData"
+       border
+       :cell-class-name="cellClassName"
+   >
+     <el-table-column prop="name" label="名字"></el-table-column>
+     <el-table-column prop="age" label="年龄"></el-table-column>
+     <el-table-column prop="sex" label="性别"></el-table-column>
+   </el-table>
+ </div>
 </template>
 
 <script>
 export default {
   name: 'pageFour',
   data() {
-    return {}
+    return {
+      tableData:[
+        { name : 'Michael' , age : 23, sex:'male' },
+        { name : 'Jane' , age : 22 , sex:'female'},
+        { name : 'KangKang' , age : 21 , sex:'male'},
+      ]
+    }
   },
-  methods: {}
+  methods: {
+    cellClassName({row, column, rowIndex, columnIndex}){
+      console.log({row, column, rowIndex, columnIndex})
+      if(rowIndex % 2  && columnIndex % 2){
+        return 'red'
+      }
+
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.table-container{
+  width: 30%;
+  margin: 50px auto;
+}
+.red{
+  color: red;
+}
 </style>
