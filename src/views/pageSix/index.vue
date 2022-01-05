50869b250e29f14e50ef15ef3424889bad8801a6
sunzengguang <szghub@gmail.com>
2022-01-05 14:28:24 +0800

    amazing

--- src/views/pageSix/index.vue
+++ src/views/pageSix/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    pageSix
+    <div>{{obj.num}}</div>
+    <el-button @click="handleClick">点击改变对象</el-button>
+    <div v-for="({n,o,time},index) in watcherStack" :key="index">
+      <div style="display: flex;justify-content: space-around">
+        <div>newValue:{{n}}</div>
+        <div>oldValue:{{o}}</div>
+        <div>time:{{time}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,9 +16,40 @@
 export default {
   name: 'pageSix',
   data() {
-    return {}
+    return {
+      obj:{},
+      watcherStack:[]
+    }
   },
-  methods: {}
+  created() {
+    this.$set(this.obj,'num',2)
+  },
+  mounted(){
+    //用来占用JS主线程
+    let t = new Date().getTime()
+    while (Date.now() < t+500){
+      ;
+    }
+  },
+
+  methods: {
+    handleClick(){
+      console.log("1231")
+      this.obj = {num:new Date().getTime()}
+    }
+  },
+  watch:{
+    'obj.num':{
+      handler(n,o){
+        this.watcherStack.push({n,o,time:new Date().getTime()})
+      }
+    },
+    'obj.arr':{
+      handler(n,o){
+        this.watcherStack.push({n,o,time:new Date().getTime()})
+      }
+    }
+  }
 }
 </script>
 
